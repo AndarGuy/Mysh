eed507ab90439b0db05bd47d8a0ba0a533f3e16e
Mikhail <mfomenko123@ya.ru>
2019-07-19 13:10:24 +0300

    add graphics and some classes

--- app/src/main/java/me/andarguy/myish/MainActivity.java
+++ app/src/main/java/me/andarguy/myish/MainActivity.java
@@ -1,27 +1,36 @@
 package me.andarguy.myish;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.pm.ActivityInfo;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+
     BluetoothHelper bluetoothHelper = new BluetoothHelper(this, this);
+    DrawView canvasDraw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        bluetoothHelper.start();
-        bluetoothHelper.connect();
+        // bluetoothHelper.start();
+        // bluetoothHelper.connect();
+
+        canvasDraw = new DrawView(this, null);
 
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
@@ -34,110 +43,17 @@ public class MainActivity extends AppCompatActivity {
         public DrawView(Context context, AttributeSet attrs) {
             super(context, attrs);
         }
+        @SuppressLint("DrawAllocation")
         @Override
         protected void onDraw(Canvas canvas) {
             super.onDraw(canvas);
             Paint paint = new Paint();
-            paint.setColor(Color.GREEN);
-            paint.setStrokeWidth(2);
+            Bitmap cheeseBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.cheese);
+            cheeseBitmap = Bitmap.createScaledBitmap(cheeseBitmap, getResources().getDimensionPixelSize(R.dimen.cheese_height), getResources().getDimensionPixelSize(R.dimen.cheese_width), false);
+            canvas.drawBitmap(cheeseBitmap, 1f, 1f, paint);
             invalidate();
         }
     }
-
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        if (!CheckConnection.isAlive()) {
-            CheckConnection = new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    while (true) {
-                        try {
-                            if (BluetoothActiv != myBluetoothAdapter.isEnabled()) {
-                                BluetoothActiv = myBluetoothAdapter.isEnabled();
-                                runOnUiThread(BlueToothNotActive);
-                            }
-                            CheckConnection.sleep(300);
-                        } catch (InterruptedException e) {
-                        }
-                    }
-                }
-            });
-            CheckConnection.start();
-        }
-    }
-
-    private void sendData(String message) {
-        TextInfo.setText(message);
-        Log.d(TAG, "...Посылаем данные: " + message + "...");
-        try {
-            outStream.write(message.getBytes());
-        } catch (IOException e) {
-            Log.d("Fatal Error", "In onResume() and an exception occurred during write: " + e.getMessage());
-        }
-    }
-
-    public void onReverse(View view){
-        Reverse = !Reverse;
-        Toast toast;
-        if(Reverse){
-            toast = Toast.makeText(getApplicationContext(),"Reverse ON", Toast.LENGTH_SHORT);
-        } else {
-            toast = Toast.makeText(getApplicationContext(),"Reverse OFF", Toast.LENGTH_SHORT);
-        }
-        toast.show();
-    }
-
-    public void onSlow_Fast(View view){
-        Speed = !Speed;
-        Toast toast;
-        if(Speed){
-            button_speed.setText("Slow");
-            toast = Toast.makeText(getApplicationContext(),"Slow", Toast.LENGTH_SHORT);
-        } else {
-            button_speed.setText("Fast");
-            toast = Toast.makeText(getApplicationContext(),"Fast", Toast.LENGTH_SHORT);
-        }
-        toast.show();
-    }
-
-    public void Connect(View view){
-        if(isConnected) {
-            TextInfo.setText("Disconnecting...");
-            Connect.setText("connect");
-            try     {
-                if (outStream != null) {
-                    outStream.flush();
-                }
-                btSocket.close();
-            } catch (IOException e2) {}
-            isConnected=false;
-            TextInfo.setText("Disconnected");
-        } else {
-            if(myBluetoothAdapter.isEnabled()) {
-                TextInfo.setText("Connecting...");
-                Connect.setText("Disconnect");
-                Log.d(TAG, "- попытка соединения...");
-                BluetoothDevice device = myBluetoothAdapter.getRemoteDevice(address);
-                try {
-                    btSocket = device.createRfcommSocketToServiceRecord(MY_UUID);
-                    if (myBluetoothAdapter.isDiscovering()) {
-                        myBluetoothAdapter.cancelDiscovery();
-                    }
-                    Log.d(TAG, "...Соединяемся...");
-                    btSocket.connect();
-                    Log.d(TAG, "...Соединение установлено и готово к передачи данных...");
-                    outStream = btSocket.getOutputStream();
-                } catch (IOException e) {
-                    Log.d("Fatal Error", "output stream creation failed:" + e.getMessage() + ".");
-                }
-                TextInfo.setText("Connected");
-                isConnected = true;
-            }
-        }
-    }
-}
 
 
 }
--- app/src/main/java/me/andarguy/myish/Mouse.java
+++ app/src/main/java/me/andarguy/myish/Mouse.java
@@ -1,12 +1,57 @@
 package me.andarguy.myish;
 
 public class Mouse {
-    int energy, score;
-    float speed;
 
-    public Mouse(int energy, int score, float speed) {
-        this.energy = energy;
+    public final static int MIN_MUSCLE_FORCE = 0, MAX_MUSCLE_FORCE = 255;
+    public final static float MIN_SPEED = 0f, MAX_SPEED = 4f;
+    private int energy = 100, score = 0, muscleForce = 0;
+    private float speed = 1.0f, scorePerUpdate = 1;
+    private boolean isRunning = false;
+
+    public Mouse() {
+
+    }
+
+    private float getPercentFromForce(int force) {
+        return muscleForce / (MAX_MUSCLE_FORCE - MIN_MUSCLE_FORCE);
+    }
+
+    public void setRunning(boolean running) {
+        isRunning = running;
+    }
+
+    public boolean isRunning() {
+        return isRunning;
+    }
+
+    public float getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(float speed) {
         this.speed = speed;
+    }
+
+    public int getEnergy() {
+        return energy;
+    }
+
+    public void setEnergy(int energy) {
+        this.energy = energy;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
         this.score = score;
+    }
+
+    public void update(int muscleForce) {
+        if (speed > 1) isRunning = true;
+        this.muscleForce = muscleForce;
+        speed = (MAX_SPEED - MIN_SPEED) * getPercentFromForce(muscleForce);
+        score = Float.valueOf(score + (scorePerUpdate * speed)).intValue();
     }
 }
